90bf999786edd78a0baf5fa7c3b6683d20bc1813
nithinmohan <nithinmohan94@gmail.com>
2018-12-13 01:11:31 +0530

    added login/logout flow

--- router/router.go
+++ router/router.go
@@ -1,31 +1,79 @@
 package router
 
-import(
-    "net/http"
-    "fmt"
-    "github.com/gorilla/mux"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"stroom/auth"
+
+	"github.com/gorilla/mux"
 )
 
-type authenticator interface {
-	Authenticate(string) error
+var authenticator auth.AuthenticateClient
+
+func AuthenticationMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session, status := authenticator.Authenticate(r)
+		if session == nil {
+			http.Error(w, "Forbidden", status)
+			return
+		}
+		log.Printf("Authenticated user")
+		next.ServeHTTP(w, r)
+	})
 }
 
-var auth authenticator 
+func Init() {
 
-func route(w http.ResponseWriter, r *http.Request) {
-	session := r.Header.Get("x-session-id")
-	err := auth.Authenticate(session)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func Init(authClient authenticator) {
-	
-	router := mux.NewRouter();
-	router.HandleFunc("/", route)
-	auth = authClient
+	router := mux.NewRouter()
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/logout", logout).Methods("GET")
+	router.Handle("/closed_endpoint", AuthenticationMiddleware(http.HandlerFunc(closed_endpoint)))
+	router.HandleFunc("/open_endpoint", open_endpoint)
+	http.Handle("/", router)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
+}
+
+var users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+}
+
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+func closed_endpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "I guess you have logged in!")
+}
+func open_endpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is public!")
+}
+func login(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Print(err)
+		return
+	}
+
+	expectedPassword, ok := users[creds.Username]
+	if !ok || expectedPassword != creds.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	authenticator.Login(w, r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Logged in")
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	authenticator.Logout(w, r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Logged out")
 }
